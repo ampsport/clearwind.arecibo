3b398cb8161758d4a726eb5e4440eed09db20d49
Elizabeth Leddy <eleddy@eastofeaton.com>
2013-09-26 15:24:49 -0700

    get rid of import of moved modules

--- clearwind/arecibo/__init__.py
+++ clearwind/arecibo/__init__.py
@@ -1,10 +1,8 @@
-from AccessControl import allow_module, allow_class, allow_type
-from AccessControl import ModuleSecurityInfo, ClassSecurityInfo
+from AccessControl import ModuleSecurityInfo
+
 
 ModuleSecurityInfo('clearwind.arecibo.wrapper').declarePublic('arecibo')
 
-import config
-import patch
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
