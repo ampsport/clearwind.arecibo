d3e9a99441e4203799b078c03309fa88f643a349
Eric Steele <ericsteele47@gmail.com>
2013-09-26 12:06:56 -0400

    Use SchemaAdapter instead of our custom utility for adapting the form.

--- clearwind/arecibo/browser/config.py
+++ clearwind/arecibo/browser/config.py
@@ -1,8 +1,15 @@
-from zope.formlib import form
+from Products.CMFDefault.formlib.schema import ProxyFieldProperty
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 from plone.app.controlpanel.form import ControlPanelForm 
+from zope.component import adapts
+from zope.formlib import form
+from zope.i18nmessageid import MessageFactory
+from zope.interface import implements
+
 from clearwind.arecibo.interfaces import IAreciboConfiguration
 
-from zope.i18nmessageid import MessageFactory
+
 _ = MessageFactory('clearwind.arecibo')
 
 class AreciboConfigurationForm(ControlPanelForm):
@@ -11,3 +18,12 @@ class AreciboConfigurationForm(ControlPanelForm):
     description = _(u"Configure Plone to work with your Arecibo account here.")
     form_name = _(u"Arecibo settings")
     label = _(u"Arecibo settings")
+
+class AreciboControlPanelAdapter(SchemaAdapterBase):
+    adapts(IPloneSiteRoot)
+    implements(IAreciboConfiguration)
+
+    account_number = ProxyFieldProperty(IAreciboConfiguration['account_number'])
+    app_name = ProxyFieldProperty(IAreciboConfiguration['app_name'])
+    transport = ProxyFieldProperty(IAreciboConfiguration['transport'])
+    ignore_localhost = ProxyFieldProperty(IAreciboConfiguration['ignore_localhost'])
